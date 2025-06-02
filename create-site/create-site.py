c15d503e6b5720367fc446569cf2176e97d854a4
Lorenzo Keller <lorenzo.keller@synaptics.com>
2025-06-02 13:07:28 +0200

    Fix support for mixed version coding schemes
    The code can now support gits with both releases
    with schema vX.Y.Z and X.Y.Z

--- create-site/create-site.py
+++ create-site/create-site.py
@@ -138,7 +138,17 @@ def get_sorted_versions():
 
     def split_version(version_name):
         parts = re.split(r'(?<=\d)(?=[A-Za-z])|(?<=[A-Za-z])(?=\d)|[._-]', version_name)
-        return [int(part) if part.isnumeric() else part for part in parts]
+
+        splitted_version = []
+
+        for part in parts:
+            # use a tuple to make sure we never compare strings with numbers
+            if part.isnumeric():
+                splitted_version.append((0, int(part)))
+            else:
+                splitted_version.append((1, part))
+
+        return splitted_version
 
     return sorted(versions, key=split_version)
 
